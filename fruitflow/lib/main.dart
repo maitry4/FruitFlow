8cf0a24884daf832065dccaf8140fd06b70f8ab4
aayushi2212 <aayushishah2212@gmail.com>
2026-03-07 09:12:45 +0530

    moved bloc provider to main.dart

--- fruitflow/lib/main.dart
+++ fruitflow/lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:fruitflow/core/router/app_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:fruitflow/core/router/app_router.dart';
+import 'package:fruitflow/features/initial/presentation/cubit/splash_cubit.dart';
 
-Future<void> main() async {
+void main() {
   usePathUrlStrategy();
- runApp(const FruitFlowApp());
+  runApp(const FruitFlowApp());
 }
 
 class FruitFlowApp extends StatelessWidget {
   const FruitFlowApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-     return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'FruitFlow',
-      routerConfig: appRouter,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<SplashCubit>(
+          create: (_) => SplashCubit(),
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Layered',
+        routerConfig: appRouter,
+      ),
     );
   }
 }
